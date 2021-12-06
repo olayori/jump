6fbc38036676d51c76eb22321c1afe1a96ae928d
Lade Sobowale <ladberg.yori@gmail.com>
2021-12-06 03:18:38 -0500

    adding tests

--- server.js
+++ server.js
@@ -21,15 +21,20 @@ app.post('/manage_file', (req, res) => {
             console.log('File successfully downloaded');
     }) 
   } else {
-      const errmess = 'Invalid Request (No valid value for key "action" found in request)';
-      res.status(400).send(errmessage)
-      console.log(errmessage);
+      const errmess = {
+        "message" : "Invalid Request (No valid value for key 'action' found in request)"
+      }
+      res.status(400).send(errmess)
+      console.log(errmess);
   }      
 });
 
 //processing requests sent to web root
 app.post('/', (req, res) => {
-    res.status(404).send("No valid endpoint here");
+    const mess = {
+      "message" : "No valid endpoint here"
+    }
+    res.status(404).send(mess);
     console.log('Invalid endpoint');
 });
 
@@ -38,10 +43,11 @@ app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.json({
       message: err.message,
-      error: "syntax error"
     });
     console.log(err.message);
   });  
 
 //Http server 
-app.listen(80, () => console.log('Listening on port 80...')); 
+app.listen(80, () => console.log('Listening on port 80...'));
+
+module.exports = app; // for testing
